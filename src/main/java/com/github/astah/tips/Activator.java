80993f02a92657e6ac8cda77499efcada182926f
shoito <infy2c@gmail.com>
2012-10-29 23:34:15 +0900

    更新チェックの組み込み

--- src/main/java/com/github/astah/tips/Activator.java
+++ src/main/java/com/github/astah/tips/Activator.java
@@ -1,15 +1,52 @@
 package com.github.astah.tips;
 
 
+import java.io.IOException;
+import java.util.Map;
+
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.github.astah.tips.updater.AutoUpdater;
+import com.github.astah.tips.util.ConfigurationUtils;
 
 public class Activator implements BundleActivator {
-
+	private static final Logger logger = LoggerFactory.getLogger(Activator.class);
+	
 	public void start(BundleContext context) {
+		Map<String, String> config = ConfigurationUtils.load();
+		String updateCheckStr = config.get(ConfigurationUtils.UPDATE_CHECK);
+		logger.info("Are there newer versions available? " + updateCheckStr);
+		
+		if ("false".equalsIgnoreCase(updateCheckStr)) {
+			return;
+		}
+		
+		runAutoUpdater();
 	}
 
 	public void stop(BundleContext context) {
 	}
 	
+	private void runAutoUpdater() {
+		new Runnable() {
+			@Override
+			public void run() {
+				try {
+					Thread.sleep(325000);
+				} catch (InterruptedException e) {
+					logger.warn(e.getMessage(), e);
+				}
+				
+				AutoUpdater autoUpdater = new AutoUpdater();
+				try {
+					autoUpdater.check();
+				} catch (IOException e) {
+					logger.warn(e.getMessage(), e);
+				}
+			}
+		};
+	}
 }
